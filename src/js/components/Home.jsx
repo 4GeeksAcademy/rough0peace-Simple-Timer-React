691707332cb1cf0885d6b89bc882050b330d0616
Ian Garvey <ianarthurgarvey@gmail.com>
2025-05-02 23:51:44 +0000

    counter now keeps track of each second by keeping track of total seconds with useState, setInterval to increase count by +1 every 1000 milliseconds = 1 second, in the SimpleCounter component each div besides the clock-icon is a numbers place to keep track of total seconds and takes the props from getSeconds to produce someCounter and placeValue. The % remainder returns an amount that is rounded down to the largest integar less than or equal to the number produced from dividing someCounter and placeValue. So useState and setInterval together determine each count rendered each second and keep track of total count, count is passed to getSeconds, the count is divided by each placeValue to get a number and a decimal and then that is rounded down to an integar.

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,40 +1,46 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import SimpleCounter from "./SimpleCounter";
 
-//create your first component
+//first component
 function Home() {
-
-	// setInterval => determines frequency (every second) that counter changes
-	// useEffect => helps update counter through setCounter 
-	// useState => remembers current count 
 
 	// stores and updates count rendered  
 	const [counter, setCounter] = useState(0);
 
-	// updates counter
-	
+	// useEffect => helps update counter through setCounter 
 	useEffect(() => {
-        console.log("useEffect runs"); // Debugging to verify only runs once
+		console.log("useEffect runs");
 
-		// counter updates +1 every 1000 milliseconds 
-        const interval = setInterval(() => {
-            setCounter((counter) => counter + 1); 
-        }, 1000);
-		
-		console.log(counter)
+		// setInterval => determines frequency (every second) that counter changes
+		const interval = setInterval(() => {
+			setCounter((counter) => counter +1);
+		}, 1000);
 
-    }, []);
+		return () => clearInterval(interval); // Cleanup function
+	}, [counter]); 
+
+
+	// getSeconds function determines value for each numerical place in clock 
+	// Math.floor rounds down and returns largest integar less or equal to starting number 
+	// The remainder (%) operator returns the remainder left over when one operand is divided by a second operand
+	function getSeconds (someCounter, placeValue){
+		return Math.floor(someCounter / placeValue) % 10
+	};
 
 	return (
 		<>
-			<SimpleCounter />
-			{counter}
+			<SimpleCounter 
+				hundredThousandsPlace={getSeconds(counter, 100000)}
+				tenThousandsPlace={getSeconds(counter, 10000)}
+				thousandsPlace={getSeconds(counter, 1000)}
+				hundredsPlace={getSeconds(counter, 100)}
+				tensPlace={getSeconds(counter, 10)}
+				onesPlace={getSeconds(counter, 1)}
+			/>
 		</>
 	)
 };
 
 export default Home
+
--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -1,15 +1,15 @@
 //create your first component
-function SimpleCounter() {
+function SimpleCounter(props) {
 	return (
 		<div className="container">
             <div className="clock-body">
-                <div className="clock-icon"><i class="fa-regular fa-clock"></i></div>
-                <div className="seconds-hundredThousands">0</div>
-                <div className="seconds-tenThousands">0</div>
-                <div className="seconds-thousands">0</div>
-                <div className="seconds-hundreds">0</div>
-                <div className="seconds-tens">0</div>
-                <div className="seconds-ones">0</div>
+                <div className="clock-icon"><i className="fa-regular fa-clock"></i></div>
+                <div className="seconds-hundredThousands">{props.hundredThousandsPlace}</div>
+                <div className="seconds-tenThousands">{props.tenThousandsPlace}</div>
+                <div className="seconds-thousands">{props.thousandsPlace}</div>
+                <div className="seconds-hundreds">{props.hundredsPlace}</div>
+                <div className="seconds-tens">{props.tensPlace}</div>
+                <div className="seconds-ones">{props.onesPlace}</div>
             </div>
 		</div>
 	)
